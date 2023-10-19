e2cbcb0a81a4c9cd39482705680179a497259c67
Jakub Irla <irlajakub@gmail.com>
2023-10-19 12:54:25 +0200

    Earth and outer-space
    Earth - poruszanie rakietą

--- gejm/src/app/earth/earth.component.ts
+++ gejm/src/app/earth/earth.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./earth.component.scss']
 })
 export class EarthComponent {
+  ngOnInit() {
+    gsap.set(".rocket",{y:window.innerHeight / 2, x: window.innerWidth / 2})
+  }
 
+  rocketMove(e: MouseEvent) {
+    const rocket = document.querySelector('.rocket')!;
+    const rocketHeight = rocket.clientHeight;
+    const rocketWidth = rocket.clientWidth;
+    gsap.to('.rocket', {duration:1, x:e.clientX - rocketWidth / 2, y:e.clientY - rocketHeight / 2, overwrite:'auto'});
+  }
 }
