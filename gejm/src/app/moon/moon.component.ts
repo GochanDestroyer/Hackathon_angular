e5a26c7846a1f75cbdd21b9ea9ca5f8b271b5913
kostekpl987 <38163890+kostekpl987@users.noreply.github.com>
2023-10-19 10:43:39 +0200

    Update Moon

--- gejm/src/app/moon/moon.component.ts
+++ gejm/src/app/moon/moon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-moon',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./moon.component.scss']
 })
 export class MoonComponent {
-
+  land(){
+      gsap.to(".landingCraft",{duration:2, y:0})
+  }
+  ngOnInit() {
+    gsap.set(".landingCraft",{y:-1000})
+    setTimeout(this.land,1000)
+   }
 }
