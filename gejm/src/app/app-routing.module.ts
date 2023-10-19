3ecd21683901b8472e24eadf8734a7dd742ccbcb
Jakub Irla <irlajakub@gmail.com>
2023-10-19 10:17:10 +0200

    Router + ReturnComponent
    Setup routera i stworzenie componentu Return - dla cutscenki z powrotem na Ziemię

--- gejm/src/app/app-routing.module.ts
+++ gejm/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildingComponent } from './building/building.component';
+import { EarthComponent } from './earth/earth.component';
+import { OuterSpaceComponent } from './outer-space/outer-space.component';
+import { MoonComponent } from './moon/moon.component';
+import { ReturnComponent } from './return/return.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'building', component:  BuildingComponent },
+  { path: 'launch', component:  EarthComponent },
+  { path: 'outer-space', component:  OuterSpaceComponent },
+  { path: 'landing', component:  MoonComponent },
+  { path: 'return', component: ReturnComponent },
+  { path: '', redirectTo: '/building', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
